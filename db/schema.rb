74fe6c73fd863f13a70e453a6be19c417c2945c1
alhimik45 <alhimik45@gmail.com>
2015-03-23 17:55:44 +0600

    add username

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150323112503) do
+ActiveRecord::Schema.define(version: 20150323115452) do
 
   create_table "labs", force: :cascade do |t|
     t.text     "task"
@@ -43,9 +43,11 @@ ActiveRecord::Schema.define(version: 20150323112503) do
     t.text     "likes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
