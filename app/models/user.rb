e4ee18d010d6e1c70fb404c9744dbe80184a4416
alhimik45 <alhimik45@gmail.com>
2015-03-23 18:12:52 +0600

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ActiveRecord::Base
          :rememberable, :trackable, :validatable
 
   has_many :sites
+
+  def email_required?
+  	false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,13 @@ Rails.application.routes.draw do
   # get 'labs' => 'labs#index'
   # get 'labs/:id' => 'labs#show'
   resources :labs
+  root 'labs#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
