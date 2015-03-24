e9abe4e39fed179e4a3ad368d333a7a39de1fe9d
alhimik45 <alhimik45@gmail.com>
2015-03-24 14:31:14 +0600

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  private
+  def after_sign_out_path_for(r)
+    request.referrer
+  end
 end
--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,14 +1,16 @@
 class SitesController < ApplicationController
 	before_action :authenticate_user!, only: [:like]
 	def like
-		site = Site.find(params[:id])
-		if current_user.has_like_for? site
-			site.likes += 1
-			site.save
-			current_user.like site
-		else
-			flash[:notice] = 'Вы уже голосовали за эту работу или Вы проголосовали за максимальное количество работ в этой лабе'
+		site = Site.find_by_id(params[:id])
+		if site
+			if current_user.has_like_for? site
+				site.likes += 1
+				site.save
+				current_user.like site
+			else
+				flash[:notice] = 'Вы уже голосовали за эту работу или Вы проголосовали за максимальное количество работ в этой лабе'
+			end
 		end
-		redirect_to :back
+		redirect_to request.referer || :root
 	end
 end
