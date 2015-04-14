0246d15dbb027ca871f1299711602bb1ffe1c889
alhimik45 <alhimik45@gmail.com>
2015-04-14 18:38:40 +0600

    refactoring

--- app/assets/javascripts/lab.js
+++ app/assets/javascripts/lab.js
@@ -26,19 +26,3 @@ function formIn(){
 	}
 	
 }
-
-function like (el, site_id) {
-	$.post("/sites/like/"+site_id);
-	el = $(el);
-	var classname = "like ";
-	var counter = el.parent().parent().find(".like-counter");
-	
-	if(el.hasClass("pressed")){
-		counter.html(+counter.html()-1);
-	}else {
-		classname += "pressed";
-		window.eee=$(el);
-		counter.html(+counter.html()+1);
-	}
-	$(el).attr('class',classname);
-}
